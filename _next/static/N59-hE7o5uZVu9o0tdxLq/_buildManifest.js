29a3b35e6051747fe5228e5b4b3a8f8e46201825
tcpglobalc <tcpglobal@protonmail.com>
2022-02-26 12:11:59 +0800

    update

--- _next/static/N59-hE7o5uZVu9o0tdxLq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/366-2edb998cc835079e.js","static/chunks/pages/index-b6f47f5f455e6f7c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
